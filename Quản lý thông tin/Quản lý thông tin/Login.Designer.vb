440838f718179714df10a2b0ee572c34fe010df4
PS01823 <anhkiet15061993@gmail.com>
2015-04-13 17:19:41 +0700

    Revert "Revert "File code""
    This reverts commit 85375f56823787d55ac2f59645102f9aea20a803.

---
+++ Quản lý thông tin/Quản lý thông tin/Login.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Login
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Login))
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtTaikhoang = New System.Windows.Forms.TextBox()
+        Me.txtmatKhau = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label17.Location = New System.Drawing.Point(33, 64)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(148, 16)
+        Me.Label17.TabIndex = 40
+        Me.Label17.Text = "Tài Khoảng Đăng Nhập"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(33, 90)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 16)
+        Me.Label1.TabIndex = 41
+        Me.Label1.Text = "Mật Khẩu"
+        '
+        'txtTaikhoang
+        '
+        Me.txtTaikhoang.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTaikhoang.Location = New System.Drawing.Point(197, 61)
+        Me.txtTaikhoang.Name = "txtTaikhoang"
+        Me.txtTaikhoang.Size = New System.Drawing.Size(123, 22)
+        Me.txtTaikhoang.TabIndex = 1
+        '
+        'txtmatKhau
+        '
+        Me.txtmatKhau.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtmatKhau.Location = New System.Drawing.Point(197, 90)
+        Me.txtmatKhau.Name = "txtmatKhau"
+        Me.txtmatKhau.Size = New System.Drawing.Size(123, 22)
+        Me.txtmatKhau.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(106, 117)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Đăng Nhập"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial Narrow", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(211, 29)
+        Me.Label2.TabIndex = 54
+        Me.Label2.Text = "Đăng Nhập Hệ Thống"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.Location = New System.Drawing.Point(326, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(130, 128)
+        Me.PictureBox1.TabIndex = 55
+        Me.PictureBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(197, 117)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 56
+        Me.Button2.Text = "Thoát"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gainsboro
+        Me.ClientSize = New System.Drawing.Size(448, 148)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtmatKhau)
+        Me.Controls.Add(Me.txtTaikhoang)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label17)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Login"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Login"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label17 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtTaikhoang As System.Windows.Forms.TextBox
+    Friend WithEvents txtmatKhau As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+End Class
---
+++ Quản lý thông tin/Quản lý thông tin/Login.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Data.SqlClient ' Câu lệnh truy cập thư viên
+Public Class Login
+    'Sự kiện sao khi nhấp vào button đăng nhập
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim chuoiketnoi As String = "workstation id=anhkietcm4.mssql.somee.com;packet size=4096;user id=anhkietcm20;pwd=Abc01091989491;data source=anhkietcm4.mssql.somee.com;persist security info=False;initial catalog=anhkietcm4 "
+
+        Dim KetNoi As SqlConnection = New SqlConnection(chuoiketnoi)
+        Dim sqlAdapter As New SqlDataAdapter("select * from NhanVien where taikhoan='" & txtTaikhoang.Text & "' and matkhau='" & txtmatKhau.Text & "' ", KetNoi)
+        Dim tb As New DataTable
+
+        Try
+            KetNoi.Open()
+            sqlAdapter.Fill(tb)
+            If tb.Rows.Count > 0 Then
+                Frmmain.Show()
+                Me.Hide()
+            Else
+                MessageBox.Show("Sai tài khoản hoặc mật khẩu")
+            End If
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    'Sự kiện sao khi nhấp vào nút thoát
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+
+End Class
